1a9c1ab97744af58d97226a9ee464cf50af41185
Arthur Molinos <arthur.molinos@gmail.com>
2021-07-08 19:07:18 +0200

    Add Gatsby

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+};
+
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <div>GATSBY CMS BOILERPLATE</div>
+    </main>
+  );
+};
+
+export default IndexPage;
