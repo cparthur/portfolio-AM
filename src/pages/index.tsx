cff0f6fc086d160f25c9265cf0856f82b71bedd5
Arthur Molinos <arthur.molinos@gmail.com>
2021-07-08 19:14:38 +0200

    Add prettier config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import * as React from "react";
+import * as React from 'react';
 
 const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+    color: '#232129',
+    padding: 96,
+    fontFamily: '-apple-system, Roboto, sans-serif, serif',
 };
 
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <div>GATSBY CMS BOILERPLATE</div>
-    </main>
-  );
+    return (
+        <main style={pageStyles}>
+            <div>GATSBY CMS BOILERPLATE</div>
+        </main>
+    );
 };
 
 export default IndexPage;
