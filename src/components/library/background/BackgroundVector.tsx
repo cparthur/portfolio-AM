ffffdc2b0967fd6c76927a4bba3cd488bf8f60cf
Arthur Molinos <arthur.molinos@gmail.com>
2021-09-28 13:08:59 +0200

    Add logo and icon assets

---
+++ src/components/library/background/BackgroundVector.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BackgroundVector = () => (
+    <svg
+        id="background-vector"
+        preserveAspectRatio="none"
+        viewBox="0 0 1440 594"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M1 239.927C178.959 169.381 73.6668 341.992 337.64 284.455C601.613 226.918 357.413 403.532 606.556 542.621C855.7 681.711 986.203 510.601 901.673 299.465C817.142 88.329 1037.61 -60.7671 1161.69 26.289C1260.95 95.9338 1388.59 95.667 1440 86.828" />
+    </svg>
+);
+
+export default BackgroundVector;
---
+++ src/components/library/icons/IconProductHunt.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const IconProductHunt = () => (
+    <svg className="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M24 12C24 18.6276 18.6276 24 12 24C5.3724 24 0 18.6276 0 12C0 5.3724 5.3724 0 12 0C18.6276 0 24 5.3724 24 12ZM9 16.8V7.19998H13.524C15.333 7.19998 16.8 8.66698 16.8 10.5C16.8 12.285 15.333 13.752 13.524 13.8H10.8V16.8H9ZM10.8 12H13.5462C14.349 12 15 11.328 15 10.5C15 9.67198 14.349 8.99998 13.5462 8.99998H10.8V12Z"
+        />
+    </svg>
+);
+
+export default IconProductHunt;
