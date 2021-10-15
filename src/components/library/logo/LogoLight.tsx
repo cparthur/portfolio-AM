79a8c30c52fbd5c80feaefe236657e97ff43cfd1
Arthur Molinos <arthur.molinos@gmail.com>
2021-10-15 19:41:21 +0200

    Add LogoLight icon component

--- src/components/library/logo/LogoLight.tsx
+++ src/components/library/logo/LogoLight.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-const LogoDark = () => (
+const LogoLight = () => (
     <svg
-        className="logo logo-dark"
+        className="logo logo-light"
         width="96"
         height="96"
         viewBox="0 0 96 96"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
     >
-        <g filter="url(#filter0_d)">
+        <g filter="url(#filter0_d_426:552)">
             <path
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M20.2257 60.4712C20.2257 59.4989 21.0151 58.7107 21.9889 58.7107H25.1491C25.4105 58.7107 25.6505 58.5663 25.7727 58.3355L46.6207 18.9382C46.9992 18.2229 47.8174 17.8572 48.604 18.0519C49.3906 18.2466 49.9429 18.9516 49.9429 19.7607V58.0065C49.9429 58.3955 50.2587 58.7107 50.6482 58.7107H54.3447C54.7342 58.7107 55.05 58.3955 55.05 58.0065L55.05 19.7607C55.05 18.9344 55.6255 18.2193 56.4337 18.0414C57.2419 17.8636 58.0652 18.2709 58.4131 19.0206L67.6374 38.8998L76.63 19.0356C76.9721 18.2798 77.7962 17.865 78.6082 18.0397C79.4202 18.2144 80 18.9313 80 19.7607L80 77.2392C80 78.2115 79.2106 78.9997 78.2367 78.9997C77.2629 78.9997 76.4735 78.2115 76.4735 77.2392L76.4735 31.1977C76.4735 30.4395 75.4382 30.2168 75.1255 30.9076L69.264 43.8555C68.9799 44.4831 68.3553 44.8877 67.6655 44.8909C66.9757 44.8941 66.3473 44.4954 66.0573 43.8705L59.9217 30.6477C59.6041 29.9631 58.5765 30.1892 58.5765 30.9438L58.5765 60.4712C58.5765 60.9381 58.3907 61.3859 58.06 61.7161C57.7294 62.0462 57.2809 62.2317 56.8132 62.2317H50.6482C50.2587 62.2317 49.9429 62.547 49.9429 62.9359V77.2392C49.9429 78.2115 49.1535 78.9997 48.1797 78.9997C47.2059 78.9997 46.4164 78.2115 46.4164 77.2392V62.9359C46.4164 62.547 46.1007 62.2317 45.7111 62.2317H28.1244C27.863 62.2317 27.6229 62.3761 27.5008 62.6069L19.3226 78.0616C18.8677 78.9213 17.8009 79.2499 16.9399 78.7957C16.0789 78.3415 15.7497 77.2764 16.2046 76.4167L23.1642 63.2649C23.4124 62.7959 23.0718 62.2317 22.5406 62.2317H21.9889C21.0151 62.2317 20.2257 61.4435 20.2257 60.4712ZM30.1093 57.6776C29.8611 58.1466 30.2017 58.7107 30.7329 58.7107H45.7111C46.1007 58.7107 46.4164 58.3955 46.4164 58.0065L46.4164 29.7016C46.4164 28.967 45.4312 28.7231 45.0875 29.3726L30.1093 57.6776Z"
-                fill="#26FF97"
+                fill="url(#paint0_linear_426:552)"
             />
         </g>
         <defs>
             <filter
-                id="filter0_d"
+                id="filter0_d_426:552"
                 x="0"
                 y="2"
                 width="96"
@@ -37,12 +37,23 @@ const LogoDark = () => (
                 <feOffset />
                 <feGaussianBlur stdDeviation="8" />
                 <feComposite in2="hardAlpha" operator="out" />
-                <feColorMatrix type="matrix" values="0 0 0 0 0.14902 0 0 0 0 1 0 0 0 0 0.592157 0 0 0 0.5 0" />
-                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
-                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+                <feColorMatrix type="matrix" values="0 0 0 0 0.556863 0 0 0 0 0.603922 0 0 0 0 1 0 0 0 0.33 0" />
+                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_426:552" />
+                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_426:552" result="shape" />
             </filter>
+            <linearGradient
+                id="paint0_linear_426:552"
+                x1="16"
+                y1="18"
+                x2="76.9298"
+                y2="81.9263"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#8E9AFF" />
+                <stop offset="1" stopColor="#CA71EA" />
+            </linearGradient>
         </defs>
     </svg>
 );
 
-export default LogoDark;
+export default LogoLight;
