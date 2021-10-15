3ab19e1f1b0429df3e42ffe7679eb4edaf49d54f
Arthur Molinos <arthur.molinos@gmail.com>
2021-10-15 19:41:04 +0200

    Add ThemeContextTypes to ThemeStore context declaration

--- src/styles/theme/ThemeStore.tsx
+++ src/styles/theme/ThemeStore.tsx
@@ -4,7 +4,12 @@ import { DefaultTheme } from 'styled-components';
 import { baseTheme, darkTheme, lightTheme } from './themeData';
 import { ThemeName } from './types';
 
-const ThemeContext = React.createContext({} as any);
+interface ThemeContextTypes {
+    switchTheme: (themeName: ThemeName) => void;
+    theme: DefaultTheme;
+}
+
+const ThemeContext = React.createContext<ThemeContextTypes>({} as ThemeContextTypes);
 
 const ThemeStore: React.FC = ({ children }) => {
     const [theme, setTheme] = useState<DefaultTheme>({ ...baseTheme, ...darkTheme });
